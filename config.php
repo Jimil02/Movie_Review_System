0cbbe6ca16bd94c78296abf0e7675467740ac1f1
JainishParekh <jainishparekh21@gmail.com>
2023-04-22 14:40:42 +0530

    config file mysql connection eastablished

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
 
-    $conn = mysqli_connect('localhost','root','','UserDirectory') or die("Unable to connect");
+
+try {
+    $conn = mysqli_connect('localhost', 'root', '', 'UserDirectory') or die("Unable to connect");
+}
+
+//catch exception
+catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
+}
     // echo "great work!!";
 ?>
